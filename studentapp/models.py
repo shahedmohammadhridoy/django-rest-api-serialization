e2eb030f1a747b7f8bd4fa896507317d33058b6e
Shahed Mohammad Hridoy <shahed_hridoy@outlook.com>
2022-11-15 16:34:24 +0600

    requirements

--- studentapp/models.py
+++ studentapp/models.py
@@ -3,3 +3,6 @@ from django.db import models
 class Student(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField(max_length=254)
+
+    def __str__(self):
+        return self.name
